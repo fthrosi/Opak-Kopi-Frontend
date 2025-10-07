bc79f10a45d03de647a89cc9cf120d5ebb19d304
kucing kalem <frosi551@gmail.com>
2025-10-07 23:20:40 +0700

    integrasi back end and yupdate .env

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "@/components/store/useAuthStore";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
