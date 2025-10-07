534d65c6f7c6f1eae1e00d8c2c95417f55ee6991
kucing kalem <frosi551@gmail.com>
2025-10-07 22:51:18 +0700

    integrasi back end and yupdate .env

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,21 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    proxy: {
+      // REST API
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      },
+      '/socket.io': {
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
+        ws: true,
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
