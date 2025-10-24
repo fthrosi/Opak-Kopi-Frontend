54b4f52e299640b05993e1d01bf2e5ebb0f71325
kucing kalem <frosi551@gmail.com>
2025-10-24 21:42:32 +0700

    make flag for active menu

--- src/components/molecules/navigation.tsx
+++ src/components/molecules/navigation.tsx
@@ -1,6 +1,7 @@
 import { Text, type TextVariantProps } from "../atoms/text";
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
+import { useLocation } from "react-router-dom";
 
 const navigationVariants = cva("flex flex-col w-fit", {
   variants: {
@@ -29,13 +30,7 @@ export interface NavigationVariantProps
   className?: string;
   textprops?: Pick<
     TextVariantProps<React.ElementType>,
-    | "className"
-    | "family"
-    | "textColor"
-    | "size"
-    | "as"
-    | "weight"
-    | "position"
+    "className" | "family" | "textColor" | "size" | "as" | "weight" | "position"
   >;
   data?: navigation[];
 }
@@ -46,17 +41,23 @@ export default function Navigation({
   data,
   textprops,
 }: NavigationVariantProps) {
+  const location = useLocation();
+  const activeMenu = location.pathname;
+
   return (
     <nav className={cn(navigationVariants({ layout, gap }), className)}>
       {data?.map((item) => (
-        <Text
-          key={item.id}
-          as="a"
-          href={item.path}
-          {...textprops}
-        >
-          {item.title}
-        </Text>
+        <div className={cn("flex-1 h-full cursor-pointer transition-all duration-300 px-2 py-4 relative",
+            "before:content-[''] before:absolute before:bottom-0 before:left-1/2 before:transform before:-translate-x-1/2",
+            "before:h-0.5 before:bg-primary before:transition-all before:duration-300",
+            activeMenu === item.path
+              ? "before:w-full before:bg-primary"
+              : "before:w-0 hover:before:w-full"
+            )} key={item.id}>
+          <Text as="a" href={item.path} {...textprops} className={`${activeMenu === item.path ? "text-primary" : "text-secondary"}`}>
+            {item.title}
+          </Text>
+        </div>
       ))}
     </nav>
   );
